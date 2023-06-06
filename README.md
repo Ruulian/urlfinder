e9e8b432f069e608a4fe364c818f0972e85fae05
Ruulian <ruulian@protonmail.com>
2023-06-06 13:26:17 +0200

    Version 1.0

--- README.md
+++ README.md
@@ -1,1 +1,91 @@
-# urlfinder
+# URLFinder
+
+## Description
+
+URLFinder is a tool developed in Python by Ruulian which attempt to find all the urls which can be found on a website. It will curl all the pages found and do the process until it found all possible urls. You can combine this tool with others to complete your recon.
+
+## Installation
+
+```
+git clone https://github.com/Ruulian/urlfinder.git
+cd urlfinder/src
+./finder.py -h
+```
+
+## Usage
+
+![help_image](images/help_image.png)
+
+## Example
+
+```
+ruulian@urlfinder-env:~$ ./finder.py -t https://0xhorizon.eu
+
+          _    _      _ ______ _           _           
+         | |  | |    | |  ____(_)         | |          
+         | |  | |_ __| | |__   _ _ __   __| | ___ _ __ 
+         | |  | | '__| |  __| | | '_ \ / _` |/ _ \ '__|
+         | |__| | |  | | |    | | | | | (_| |  __/ |   
+          \____/|_|  |_|_|    |_|_| |_|\__,_|\___|_|    by Ruulian
+
+        Version: 1.0
+        
+==========================================================================
+[STATUS_CODE] url
+[200] https://0xhorizon.eu
+[200] https://0xhorizon.eu/
+[200] https://0xhorizon.eu/assets/images/logo_Horiz0nx.png
+[200] https://0xhorizon.eu/index.xml
+[200] https://0xhorizon.eu/index.json
+[200] https://0xhorizon.eu/fr/
+[200] https://0xhorizon.eu/assets/images/logo_thumbnail.png
+[200] https://0xhorizon.eu/writeups/
+[200] https://0xhorizon.eu/articles/
+[200] https://0xhorizon.eu/cheat-sheet/
+[200] https://0xhorizon.eu/search/
+[200] https://0xhorizon.eu/articles/rsa/
+[200] https://0xhorizon.eu/cheat-sheet/xss/
+[200] https://0xhorizon.eu/articles/xss/
+[200] https://0xhorizon.eu/articles/xss-stored/
+[200] https://0xhorizon.eu/articles/xss-reflected/
+[200] https://0xhorizon.eu/articles/xss-dom-based/
+[200] https://0xhorizon.eu/writeups/bouncy-box/
+[200] https://0xhorizon.eu/writeups/ssti/
+[200] https://0xhorizon.eu/articles/http-response-splitting/
+[200] https://0xhorizon.eu/writeups/cross_dungeon/
+[200] https://0xhorizon.eu/writeups/fantasy_book/
+[200] https://0xhorizon.eu/writeups/fantasy_gallery/
+[200] https://0xhorizon.eu/writeups/my_first_treasure/
+[200] https://0xhorizon.eu/writeups/quotehub/
+[200] https://0xhorizon.eu/fr/index.xml
+[200] https://0xhorizon.eu/fr/index.json
+[200] https://0xhorizon.eu/fr/writeups/
+[200] https://0xhorizon.eu/fr/articles/
+[200] https://0xhorizon.eu/fr/cheat-sheet/
+[200] https://0xhorizon.eu/fr/search/
+[200] https://0xhorizon.eu/writeups/index.xml
+[200] https://0xhorizon.eu/writeups/page/2/
+[200] https://0xhorizon.eu/articles/index.xml
+[200] https://0xhorizon.eu/articles/page/2/
+[200] https://0xhorizon.eu/cheat-sheet/index.xml
+[200] https://0xhorizon.eu/fr/articles/rsa/
+[200] https://0xhorizon.eu/fr/cheat-sheet/xss/
+[200] https://0xhorizon.eu/fr/articles/xss/
+[200] https://0xhorizon.eu/fr/articles/xss-stored/
+[200] https://0xhorizon.eu/fr/articles/xss-reflected/
+[200] https://0xhorizon.eu/fr/articles/xss-dom-based/
+[200] https://0xhorizon.eu/fr/writeups/bouncy-box/
+[200] https://0xhorizon.eu/fr/writeups/ssti/
+[200] https://0xhorizon.eu/fr/articles/http-response-splitting/
+[200] https://0xhorizon.eu/fr/writeups/cross_dungeon/
+[200] https://0xhorizon.eu/fr/writeups/fantasy_book/
+[200] https://0xhorizon.eu/fr/writeups/fantasy_gallery/
+[200] https://0xhorizon.eu/fr/writeups/my_first_treasure/
+[200] https://0xhorizon.eu/fr/writeups/quotehub/
+[200] https://0xhorizon.eu/fr/writeups/index.xml
+[200] https://0xhorizon.eu/fr/writeups/page/2/
+[200] https://0xhorizon.eu/fr/articles/index.xml
+[200] https://0xhorizon.eu/fr/articles/page/2/
+[200] https://0xhorizon.eu/fr/cheat-sheet/index.xml
+==========================================================================
+```
--- src/finder.py
+++ src/finder.py
@@ -1,22 +1,72 @@
+#!/usr/bin/python3
+
+import argparse
 import requests
 from customqueue import CustomQueue
 from urllib.parse import urlparse
-from display import Display
 import re
+import time
+import urllib3
+from sys import stdout
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 class Finder:
-    def __init__(self, base_url):
+    def __init__(self, base_url, cookies={}, headers={}, sleep=0.0, output_file="", no_colors=False):
         self.base_url = base_url
+
+        self.cookies = cookies
+        self.headers = headers
+        self.sleep = sleep
+        self.output_file = output_file
+        self.no_colors = no_colors
+
         self.domain = Finder.get_domain(self.base_url)
         self.urls = CustomQueue()
 
-    def ping(url):
+    # Display
+
+    def banner():
+        print(f"""
+          _    _      _ ______ _           _           
+         | |  | |    | |  ____(_)         | |          
+         | |  | |_ __| | |__   _ _ __   __| | ___ _ __ 
+         | |  | | '__| |  __| | | '_ \ / _` |/ _ \ '__|
+         | |__| | |  | | |    | | | | | (_| |  __/ |   
+          \____/|_|  |_|_|    |_|_| |_|\__,_|\___|_|   \x1b[0m\x1b[3m by Ruulian\x1b[0m
+
+        \x1b[4mVersion\x1b[0m: 1.0
+        """)
+        
+    
+    def print(file, message="", no_colors=False):
+        if no_colors:
+            message = re.sub("\x1b[\[]([0-9;]+)m", "", message)
+        
+        file.write(f"{message}\n")
+
+    def display_url(file, status_code, url, no_colors=False):
+        if status_code > 0 and (status_code < 200 or (300 <= status_code and status_code < 400)):
+            color = 33
+        elif 200 <= status_code and status_code < 300:
+            color = 32
+        else:
+            color = 31
+
+        if status_code == -1:
+            status_code = "ERROR"
+            
+        Finder.print(file, f"[\x1b[{color}m{status_code}\x1b[0m] {url}", no_colors)
+
+    # Finder behavior
+
+    def ping(self, url):
         try:
-            r = requests.get(url)
+            r = requests.get(url, cookies=self.cookies, headers=self.headers, allow_redirects=True, verify=False)
             return (r.status_code, r.text)
         
         except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError):
-            return (404, "")
+            return (-1, "")
 
     def check_presence(self, url) -> bool:
         return url in self.urls
@@ -39,9 +89,13 @@ class Finder:
         while not q.empty():
             url = q.get()
 
-            status_code, text = Finder.ping(url)
+            status_code, text = self.ping(url)
 
-            Display.display_url(status_code, url)
+            Finder.display_url(stdout, status_code, url)
+
+            if self.output_file != "":
+                with open(self.output_file, "a") as f:
+                    Finder.display_url(f, status_code, url, no_colors=True)
 
             found_urls = self.get_links(text)
 
@@ -49,8 +103,43 @@ class Finder:
                 if self.check_domain(u) and not self.check_presence(u):
                     q.put(u)
                     self.urls.put(u)
+            time.sleep(self.sleep)
 
+def parse_dict_arg(arg:str):
+    cookies = {}
+    cookies_arg = arg.split(";")
+    for c in cookies_arg:
+        cookie = c.split("=")
+        try:
+            cookies[cookie[0]] = cookie[1]
+        except IndexError:
+            raise argparse.ArgumentTypeError("Cookies must be specified with key=value")
+    return cookies
 
-# TODO : Enable subdomain
-f = Finder("https://root-me.org/")
-f.traverse()
+def parse_args():
+    parser = argparse.ArgumentParser(add_help=True, description='URLFinder tool')
+
+    required_args = parser.add_argument_group("Required argument")
+    required_args.add_argument("-t", "--target", dest="target", help="Specify the target url", required=True)
+
+    setup_args = parser.add_argument_group("Setup")
+    setup_args.add_argument("-c", "--cookies", dest="cookies", help="Specify the cookies key1=value1;key2=value2", type=parse_dict_arg, required=False, default={})
+    setup_args.add_argument("-H", "--headers", dest="headers", help="Specify the headers key1=value1;key2=value2", type=parse_dict_arg, required=False, default={})
+    
+    setup_args.add_argument("-o", "--output", dest="output_file", help="Specify output file", type=str, required=False, default="")
+    setup_args.add_argument("-s", "--sleep", dest="sleep", help="Specify the sleep between the requests", type=float, required=False, default=0.0)
+
+    setup_args.add_argument("--no-colors", dest="no_colors", action="store_true", help="Disable color mode")
+    
+    
+    args = parser.parse_args()
+    return args
+
+if __name__ == '__main__':
+    Finder.banner()
+    args = parse_args()
+    finder = Finder(args.target, args.cookies, args.headers, args.sleep, args.output_file, args.no_colors)
+    Finder.print(stdout, "="*74)
+    Finder.print(stdout, "[STATUS_CODE] url")
+    finder.traverse()
+    Finder.print(stdout, "="*74)
